0d99ec9b46fe4dd37ea13061d8de6b31104757d1
leankuey <2306359@escolas.anchieta.br>
2024-11-20 15:54:50 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -7,7 +7,7 @@ st.title("Arte & Tinta")
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("COLORINDO A SUA OBRA.")
 
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("Batata doce")
 
 prompt = st.chat_input("Say something")
 if prompt:
