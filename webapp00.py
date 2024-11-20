864ed0e959fc60ddcd474bc889e913f38dadb26e
leankuey <2306359@escolas.anchieta.br>
2024-11-20 20:12:11 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -2,7 +2,7 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-                                    st.title("Arte & Tinta")
+st.title("Arte & Tinta")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("COLORINDO A SUA OBRA.")
