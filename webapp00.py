98221a7f2867b9f60af0c5f8857baacc40e3ead9
leankuey <2306359@escolas.anchieta.br>
2024-11-23 18:39:35 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -7,7 +7,7 @@ st.title("Arte & Tinta")
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("COLORINDO A SUA OBRA.")
 
-import streamlit as st
+st.caption("Este aplicativo foi criado para ajudar você a calcular a quantidade de tinta necessaria para a sua pintura de forma rápida e prática.")
 
 # Entrada do usuário
 altura = st.number_input("Digite a altura da parede (em metros):", min_value=0.0, format="%.2f")
@@ -20,6 +20,5 @@ if altura > 0 and comprimento > 0:
 else:
     st.info("Por favor, insira valores válidos para altura e comprimento.")
 
-# Nota opcional
-st.caption("Este aplicativo foi criado para ajudar você a calcular a área de paredes de forma rápida e prática.")
+
 
